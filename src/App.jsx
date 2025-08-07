6d97fa0fa57654c690bb0de1368c42a3965cfc34
Danyel Gomes <danyeldev1@gmail.com>
2025-08-07 10:15:51 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import img from '../src/images.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +8,7 @@ function App() {
   return (
     <main>
       <h1>Install Elden Ring Bro...</h1>
-      <img src='/src/images.jpg'/>
+      <img src={img}/>
       <a href='https://hydralauncher.gg/' target='blank' >Download</a>
     </main>
   )
